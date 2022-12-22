a50aa6b03f1409481b05fe290af5c4f7db05ac7e
Salvatore Tocco <you@example.com>
2022-12-21 21:15:04 -0500

    email.js

--- utils/email.js
+++ utils/email.js
@@ -1,25 +1,30 @@
 const nodemailer = require('nodemailer')
 
-let mailTransporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth:{
-        user: 'process.env',
-        pass: 'process.env'
-    }
-})
+function sendMail(to, subject, text) {
 
-let details = {
-    from: 'process.env',
-    to: 'test.gmail.com',
-    subject: 'testing',
-    text: 'testing out first sender'
+    let mailTransporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth:{
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    })
+
+    let details = {
+        from: process.env.EMAIL_USER,
+        to,
+        subject,
+        text
+    }
+
+    mailTransporter.sendMail(details, (err) => {
+        if(err){
+            console.log('error', err)
+        }
+        else{
+            console.log('email has been sent!')
+        }
+    })
 }
 
-mailTransporter.sendMail(details, (err) => {
-    if(err){
-        console.log('error', err)
-    }
-    else{
-        console.log('email has been sent!')
-    }
-})
+
