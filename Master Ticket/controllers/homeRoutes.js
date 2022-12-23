155982976d952d915ec2cb67b13e5aebdb4a3ab2
Cody Hunt <codyrhunt77@gmail.com>
2022-12-22 22:39:48 -0500

    added login
    added login create account files

---
+++ Master Ticket/controllers/homeRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const  User  = require('../models/User');
+const withAuth = require('../utils/auth');
+router.get('/' , withAuth , async (req , res) => {
+    try {
+        const userData = await User.findAll({
+            attributes: { exclude: ['password'] },
+            order: [['name' , 'ASC']],
+        });
+        const user = userData.map((market) => market.get({ plain: true}));
+        res.render('main', {
+            user, logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+router.get('/login' , async (req, res) => {
+    console.log('please work')
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+module.exports = router;
